97b6f450ad2624e89801a801d2362f58637adb44
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-06 22:39:47 -0500

    Creando la clase MySQLdb para realizar la conexión hacia la base de datos

---
+++ app/libs/MySQLdb.php
@@ -0,0 +1,24 @@
+<?php
+    class MySQLdb {
+
+        private $host="localhost";
+        private $usuario = "root";
+        private $clave = "";
+        private $db = "taller";
+        private $puerto = "";
+        private $conn;
+
+        function __construct() {
+           try {
+            $this->conn = new PDO(
+                'mysql:host='.$this->host.';dbname='.$this->db, 
+                $this->usuario,
+                $this->clave
+            );
+            echo "Conectado";
+           } catch (Exception $e) {
+                die("No se pudo conectar: ".$e->getMessage());
+           }
+        }
+    }
+?>
