9930c0529581a66e324cf7bce8f24f9da141d273
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-07 22:33:18 -0500

    Creando el método separarURL en el cual se realiza validaciones para eliminar caracteres especiales y sanitizar lo que se ingrese desde el navegador

--- app/libs/Control.php
+++ app/libs/Control.php
@@ -5,7 +5,21 @@
         private $parametros = [1,2,3];
 
         function __construct() {
-            print "<h2>Bienvenidos al taller mecánico</h2>";
+            $url = $this->separarURL();
+            Helper::mostrar($url);
         }
+
+        public function separarURL():array {
+            $url = [];
+            if(isset($_GET['url'])) {
+                // eliminar el caracter final
+                $url = rtrim($_GET['url'], "/");
+                $url = rtrim($_GET['url'], "\\");
+                // sanitizar (quita todos los caracteres que no pertenezcan al inglés)
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode("/",$url);
+            }
+            return $url;
+        } 
     }
 ?>
