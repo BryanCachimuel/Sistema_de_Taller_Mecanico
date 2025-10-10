c03fcf1b32ae3993d5e1195eb38b9d92a330235b
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-09 22:21:43 -0500

    Creando la función vista para dar validaciones de que existan las vistas y su respectiva redirección adecuada dentro de cada controlador

--- app/libs/Controlador.php
+++ app/libs/Controlador.php
@@ -4,12 +4,20 @@
         
         function __construct() { }
 
-        public function modelo($modelo='') {
+        public function modelo(string $modelo='') {
             if (file_exists("../app/modelos/".$modelo.".php")) {
                 require_once("../app/modelos/".$modelo.".php");
                 return new $modelo;
             }else {
                 die("El modelo ".$modelo." no existe");
+            }
+        }
+
+        public function vista($vista='',$datos=[]) {
+            if (file_exists("../app/vistas/".$vista.".php")) {
+                require_once("../app/vistas/".$vista.".php");
+            }else {
+                die("La vista ".$vista." no existe");
             }
         }
 
