08fb686d9eb8dea82f201e1455c62518ffd5bc1b
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-09 22:22:35 -0500

    Realizando un requerimiento de la clase Controlador

--- app/inicio.php
+++ app/inicio.php
@@ -1,5 +1,6 @@
 <?php
     require_once("libs/Helper.php");
+    require_once("libs/Controlador.php");
     require_once("libs/Control.php");
     require_once("libs/MySQLdb.php");
     $control = new Control();
