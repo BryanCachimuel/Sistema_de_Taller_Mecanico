07a145ff5fb56c5370351d9bb366772a924d45d1
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-09 22:35:50 -0500

    Creando dos constantes para determinar los tipos de usuarios que se manejaran dentro del sistema

--- app/inicio.php
+++ app/inicio.php
@@ -1,7 +1,10 @@
 <?php
-    require_once("libs/Helper.php");
-    require_once("libs/Controlador.php");
-    require_once("libs/Control.php");
-    require_once("libs/MySQLdb.php");
-    $control = new Control();
-?>
+// Tipos de Usuarios
+define('ADMON', 1);
+define('OPERADOR', 2);
+
+require_once("libs/Helper.php");
+require_once("libs/Controlador.php");
+require_once("libs/Control.php");
+require_once("libs/MySQLdb.php");
+$control = new Control();
