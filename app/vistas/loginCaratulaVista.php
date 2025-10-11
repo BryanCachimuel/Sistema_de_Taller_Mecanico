898e43f80b27edc860029ecb1c41bfddd6b3c046
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-10 22:33:32 -0500

    Creando la vista para el inicio de sesión

---
+++ app/vistas/loginCaratulaVista.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+	<title>Taller</title>
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		<a href="#" class="navbar-brand">Taller</a>
+	</nav>
+	<div class="container-fluid">
+		<div class="row content">
+			<div class="col-sm-1"></div>
+			<div class="col-sm-10">
+				<div class="card p-4 mt-3 bg-light">
+					<form>
+						<table>
+							<tr>
+								<td>Usuario:</td>
+								<td><input type="text" name="usuario" id="usuario" /></td>
+							</tr>
+							<tr>
+								<td>Clave:</td>
+								<td><input type="password" name="clave" id="clave" /></td>
+							</tr>
+						</table>
+						<input type="submit" value="Iniciar Sesión">
+					</form>
+				</div>
+			</div>
+			<div class="col-sm-1"></div>
+		</div>
+	</div>
+</body>
+
+</html>
