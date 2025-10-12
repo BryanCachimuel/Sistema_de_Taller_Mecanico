e422563159eee404b6e9226123b61d7b073e6668
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-11 21:26:59 -0500

    Agregando adecuadamente las rutas para el funcionamiento del formulario para la redirección del boton de regreso

--- app/vistas/loginOlvidoVista.php
+++ app/vistas/loginOlvidoVista.php
@@ -1,12 +1,12 @@
 <?php include_once("encabezado.php"); ?>
-<form action="#" method="POST">
+<form action="<?php print RUTA; ?>login/olvido/" method="POST">
     <div class="form-group text-start">
         <label for="correo">* Correo:</label>
-        <input id="correo" name="correo" type="email" class="form-control" placeholder="Escribe tu correo electrónico">
+        <input id="correo" name="correo" type="text" class="form-control" placeholder="Escribe tu correo electrónico">
     </div>
     <div class="form-group text-start my-2">
         <input type="submit" value="Enviar" class="btn btn-success">
-        <a href="login" class="btn btn-success">Regresar</a>
+        <a href="<?php print RUTA; ?>login" class="btn btn-success">Regresar</a>
     </div>
 </form>
 <p>Escribe tu correo registrado en el sistema</p>
