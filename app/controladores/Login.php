4f10c20ca5d0fcf783fca09b8ba4fd9d82fe3cb8
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-11 20:45:27 -0500

    En la función olvido se realizan validaciones en el formulario para controlar que se ingrese información dentro de los inputs especialmente en el correo

--- app/controladores/Login.php
+++ app/controladores/Login.php
@@ -18,6 +18,20 @@
         }
 
         public function olvido() {
+            $errores = [];
+            if($_SERVER['REQUEST_METHOD'] == "POST") {
+                $correo = $_POST['correo']??"";
+                if(empty($correo)){
+                    array_push($errores, "El correo electrónico es requerido");
+                }
+                if(filter_var($correo, FILTER_VALIDATE_EMAIL) == false){
+                    array_push($errores, "El correo electrónico no está bien escrito");
+                }
+                if(empty($errores)) {
+                    Helper::mostrar($correo);
+                }
+                Helper::mostrar($errores);
+            }
             $datos = [
                 "titulo" => "Olvido de la clave de Acceso",
                 "subtitulo" => "Olvidaste tu clave de acceso"
