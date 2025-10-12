9591bc2f35a042ff23a6cf212b826f92c586f291
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-11 21:09:50 -0500

    Creando la función buscarCorreo para que mediante una consulta sql se pueda obtener todos los datos de un usuario mediante su correo registrado en la base de datos

--- app/modelos/LoginModelo.php
+++ app/modelos/LoginModelo.php
@@ -9,6 +9,15 @@
             $this->db = new MySQLdb();
         }
 
+        public function buscarCorreo(string $correo=''): array {
+            if($correo == "") return [];
+            $sql = "SELECT id, tipoUsuario, nombres, apellidos, direccion, ";
+            $sql.= "telefono, correo, clave, genero, estadoUsuario ";
+            $sql.= "FROM usuarios ";
+            $sql.= "WHERE correo = '".$correo."' AND baja=0";
+            return $this->db->query($sql);
+        }
+
     }
 
 ?>
