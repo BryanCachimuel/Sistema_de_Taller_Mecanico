0d91b3ae86fc1730fa05299373757c87b225ae21
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-13 21:40:05 -0500

    Se crean dos funciones una para encriptar y otra para descencriptar, en estas dos funciones se utilizan las constantes creadas dentro del archivo inicio.php

--- app/libs/Helper.php
+++ app/libs/Helper.php
@@ -11,6 +11,25 @@
             }
         }
 
+        public static function encriptar(string $data): string {
+            return base64_encode(LLAVE1.$data.LLAVE2);
+        }
+
+        public static function desencriptar(string $data):string {
+            $cadena = base64_decode($data);
+            if(str_contains($cadena, LLAVE1)){
+                $cadena = str_replace(LLAVE1,"",$cadena);
+                if(str_contains($cadena, LLAVE2)){
+                    $cadena = str_replace(LLAVE2,"",$cadena);
+                }else {
+                    $cadena = "error";
+                }
+            }else {
+                $cadena = "error";
+            }
+            return $cadena;
+        }
+
     }
 
 ?>
