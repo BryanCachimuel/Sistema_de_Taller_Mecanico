4df0b8a6371ca32c8321d25f97e3588fc71e2bb5
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-13 22:45:35 -0500

    Creando la función para actualizarClaveAcceso mediante consulta sql a la base de datos

--- app/modelos/LoginModelo.php
+++ app/modelos/LoginModelo.php
@@ -9,6 +9,14 @@
             $this->db = new MySQLdb();
         }
 
+        public function actualizarClaveAcceso(array $data=[]):bool {
+            if(!empty($data)) {
+                $sql = "UPDATE usuarios SET clave=:clave WHERE id=:id";
+                return $this->db->queryNoSelect($sql,$data);
+            }  
+            return false; 
+        }
+
         public function buscarCorreo(string $correo=''): array {
             if($correo == "") return [];
             $sql = "SELECT id, tipoUsuario, nombres, apellidos, direccion, ";
