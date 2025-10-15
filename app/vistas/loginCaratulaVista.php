cbf1e887d412b2c8a3a7f0c490bf272f1888ad35
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-14 21:38:09 -0500

    Integrando la forma adecuada de integración de las rutas tanto para el formulario como para el botón de olvido de clave

--- app/vistas/loginCaratulaVista.php
+++ app/vistas/loginCaratulaVista.php
@@ -1,20 +1,20 @@
 <?php include_once("encabezado.php"); ?>
-						<form action="#" method="POST">
-							<div class="form-group text-start">
-								<label for="usuario">* Usuario:</label>
-								<input id="usuario" name="usuario" type="email" class="form-control" placeholder="Escribe tu usuario (correo electrónico)" required>
-							</div>
-							<div class="form-group text-start">
-								<label for="clave">* Clave de acceso:</label>
-								<input id="clave" name="clave" type="password" class="form-control" placeholder="Escribe tu clave de acceso">
-							</div>
-							<div class="form-group text-start mt-2">
-								<input type="checkbox" name="recordar">
-								<label for="recordar">Recordar</label>
-							</div>
-							<div class="form-group text-start my-2">
-								<input type="submit" value="Iniciar Sesión" class="btn btn-success">
-							</div>
-						</form>
-						<a href="/taller/login/olvido">¿Olvidaste tu clave de acceso?</a><br>
+<form action="<?php print RUTA; ?>/login/verificar" method="POST">
+	<div class="form-group text-start">
+		<label for="usuario">* Usuario:</label>
+		<input id="usuario" name="usuario" type="email" class="form-control" placeholder="Escribe tu usuario (correo electrónico)" required>
+	</div>
+	<div class="form-group text-start">
+		<label for="clave">* Clave de acceso:</label>
+		<input id="clave" name="clave" type="password" class="form-control" placeholder="Escribe tu clave de acceso">
+	</div>
+	<div class="form-group text-start mt-2">
+		<input type="checkbox" name="recordar">
+		<label for="recordar">Recordar</label>
+	</div>
+	<div class="form-group text-start my-2">
+		<input type="submit" value="Iniciar Sesión" class="btn btn-success">
+	</div>
+</form>
+<a href="<?php print RUTA; ?>/login/olvido">¿Olvidaste tu clave de acceso?</a><br>
 <?php include_once("piepagina.php"); ?>
