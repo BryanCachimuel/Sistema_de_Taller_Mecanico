93b42fa5df538d1179715df8245c3c32ff7151d9
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-15 22:28:51 -0500

    Agregando la clase Sesión dentro del archivo inicio.php

--- app/inicio.php
+++ app/inicio.php
@@ -8,6 +8,7 @@ define('ADMON', 1);
 define('OPERADOR', 2);
 
 require_once("libs/Helper.php");
+require_once("libs/Sesion.php");
 require_once("libs/Controlador.php");
 require_once("libs/Control.php");
 require_once("libs/MySQLdb.php");
