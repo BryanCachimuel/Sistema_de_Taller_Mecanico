307d8ba07450b655ea28e90e870bca9a10d808b8
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-16 21:45:02 -0500

    app/vistas/loginCaratulaVista.php

--- app/vistas/loginCaratulaVista.php
+++ app/vistas/loginCaratulaVista.php
@@ -1,20 +1,20 @@
 <?php include_once("encabezado.php"); ?>
-<form action="<?php print RUTA; ?>/login/verificar" method="POST">
-	<div class="form-group text-start">
-		<label for="usuario">* Usuario:</label>
-		<input id="usuario" name="usuario" type="email" class="form-control" placeholder="Escribe tu usuario (correo electrónico)" required>
-	</div>
-	<div class="form-group text-start">
-		<label for="clave">* Clave de acceso:</label>
-		<input id="clave" name="clave" type="password" class="form-control" placeholder="Escribe tu clave de acceso">
-	</div>
-	<div class="form-group text-start mt-2">
-		<input type="checkbox" name="recordar">
-		<label for="recordar">Recordar</label>
-	</div>
-	<div class="form-group text-start my-2">
-		<input type="submit" value="Iniciar Sesión" class="btn btn-success">
-	</div>
-</form>
-<a href="<?php print RUTA; ?>/login/olvido">¿Olvidaste tu clave de acceso?</a><br>
-<?php include_once("piepagina.php"); ?>
+	<form action="<?php print RUTA; ?>login/verificar" method="POST">
+		<div class="form-group text-start">
+			<label for="usuario">* Usuario:</label>
+			<input id="usuario" name="usuario" type="email" class="form-control" placeholder="Escribe tu usuario (correo electrónico)" value="<?php print isset($datos['data']['usuario'])?$datos['data']['usuario']:''; ?>" required>
+		</div>
+		<div class="form-group text-start">
+			<label for="clave">* Clave de acceso:</label>
+			<input id="clave" name="clave" type="password" class="form-control" placeholder="Escribe tu clave de acceso" value="<?php print isset($datos['data']['clave'])?$datos['data']['clave']:''; ?>" required>
+		</div>
+		<div class="form-group text-start mt-2">
+			<input type="checkbox" name="recordar" <?php print isset($datos['data']['usuario'])?'checked':''; ?> >
+			<label for="recordar">Recordar</label>
+		</div>
+		<div class="form-group text-start mt-2">
+			<input type="submit" value="Enviar" class="btn btn-success">
+		</div>
+		<a href="<?php print RUTA; ?>login/olvidoVerificar">¿Olvidaste tu clave de acceso?</a><br>
+	</form>
+<?php include_once("piepagina.php"); ?>				
