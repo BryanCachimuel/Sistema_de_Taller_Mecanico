6cd785e3e26a4cf7b64aa9e3b0f6536be07cf89f
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-16 21:32:20 -0500

    Agregando la variable privada usuario

--- app/controladores/Tablero.php
+++ app/controladores/Tablero.php
@@ -1,38 +1,43 @@
-<?php
-
+<?php  
+/**
+ * 
+ */
 class Tablero extends Controlador
 {
+	private $usuario = "";
     private $modelo = "";
-    private $sesion;
+	private $sesion;
+	
+	function __construct()
+	{
+		//Creamos sesion
+		$this->sesion = new Sesion();
+		if ($this->sesion->getLogin()) {
+			$this->modelo = $this->modelo("TableroModelo");
+			$this->usuario = $this->sesion->getUsuario();
+		} else {
+			header("location:".RUTA);
+		}
+	}
 
-    function __construct()
-    {
-        // se crea una sesión
-        $this->sesion = new Sesion();
-        if($this->sesion->getLogin()){
-            $this->modelo = $this->modelo("TableroModelo");
-            $this->usuario = $this->sesion->getUsuario();
-        } else {
-            header("location:".RUTA);
-        }
-    }
+	public function caratula()
+	{
+		$datos = [
+			"titulo" => "Sistema de taller mecánico",
+			"subtitulo" => $this->usuario["nombres"]." ".$this->usuario["apellidos"],
+			"usuario"=>$this->usuario,
+			"data"=>[],
+			"menu" => true
+		];
+		$this->vista("tableroCaratulaVista",$datos);
+	}
 
-    public function caratula()
-    {
-        $datos = [
-            "titulo" => "Sistema de Taller Mecánico",
-            "subtitulo" => $this->usuario["nombre"]." ".$this->usuario["apellido"],
-            "usuario" => $this->usuario,
-            "data" => [],
-            "menu" => true
-        ];
-        $this->vista("tableroCaratulaVista", $datos);
-    }
-
-  
-
-   
-
-    
-
+	public function logout()
+	{
+		if (isset($_SESSION['usuario'])) {
+			$this->sesion->finalizarLogin();
+		}
+		header("location:".RUTA);
+	}
 }
+?>
