7830358f495aada1e2b050b7f5210a4d827b8882
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-16 22:09:47 -0500

    Creando el controlador para Usuarios el cual contiene sus atributos, método constructor y caratula

---
+++ app/controladores/Usuarios.php
@@ -0,0 +1,35 @@
+<?php  
+
+class Usuarios extends Controlador {
+
+	private $usuario = "";
+    private $modelo = "";
+	private $sesion;
+	
+	function __construct() {
+		//Creamos sesion
+		$this->sesion = new Sesion();
+		if ($this->sesion->getLogin()) {
+			$this->modelo = $this->modelo("UsuariosModelo");
+			$this->usuario = $this->sesion->getUsuario();
+		} else {
+			header("location:".RUTA);
+		}
+	}
+
+	public function caratula() {
+        $data = $this->modelo->getTabla();
+        Helper::mostrar($data);
+ 		$datos = [
+			"titulo" => "Usuarios taller mecánico",
+			"subtitulo" => "Usuarios taller mecánico",
+			"usuario"=>$this->usuario,
+			"data"=> $data,
+			"menu" => true
+		];
+		$this->vista("usuariosCaratulaVista",$datos);
+	}
+
+	
+}
+?>
