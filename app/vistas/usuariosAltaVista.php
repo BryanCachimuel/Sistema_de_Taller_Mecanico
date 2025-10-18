15cc02d42fb7e0dce59087b6ab6f703c1fcfbb18
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-17 22:16:13 -0500

    Creando la vista para registrar nuevos usuarios

--- app/vistas/usuariosAltaVista.php
+++ app/vistas/usuariosAltaVista.php
@@ -1,13 +1,43 @@
 <?php include_once("encabezado.php"); ?>
-<form action="<?php print RUTA; ?>login/olvido/" method="POST">
+<form action="<?php print RUTA; ?>usuarios/alta" method="POST">
     <div class="form-group text-start">
-        <label for="correo">* Correo:</label>
-        <input id="correo" name="correo" type="text" class="form-control" placeholder="Escribe tu correo electrónico">
+        <label for="tipoUsuario">* Tipo de usuario:</label>
+        <input id="tipoUsuario" name="tipoUsuario" type="text" class="form-control" placeholder="">
     </div>
+
+    <div class="form-group text-start">
+        <label for="nombres">* Nombre del usuario:</label>
+        <input id="nombres" name="nombres" type="text" class="form-control" placeholder="Nombre del usuario" required>
+    </div>
+
+    <div class="form-group text-start">
+        <label for="direccion">Dirección:</label>
+        <input id="direccion" name="direccion" type="text" class="form-control" placeholder="Dirección del usuario">
+    </div>
+
+    <div class="form-group text-start">
+        <label for="telefono">* Telefono del usuario:</label>
+        <input id="telefono" name="telefono" type="text" class="form-control" placeholder="Teléfono del usuario" required>
+    </div>
+
+    <div class="form-group text-start">
+        <label for="correo">* Correo del usuario:</label>
+        <input id="correo" name="correo" type="text" class="form-control" placeholder="Correo del usuario" required>
+    </div>
+
+    <div class="form-group text-start">
+        <label for="genero">* Género del usuario:</label>
+        <input id="genero" name="genero" type="text" class="form-control" placeholder="Género del usuario" required>
+    </div>
+
+    <div class="form-group text-start">
+        <label for="estadoUsuario">* Género del usuario:</label>
+        <input id="estadoUsuario" name="estadoUsuario" type="text" class="form-control" placeholder="Género del usuario" required>
+    </div>
+
     <div class="form-group text-start my-2">
         <input type="submit" value="Enviar" class="btn btn-success">
-        <a href="<?php print RUTA; ?>login" class="btn btn-success">Regresar</a>
+        <a href="<?php print RUTA; ?>usuarios" class="btn btn-success">Regresar</a>
     </div>
 </form>
-<p>Escribe tu correo registrado en el sistema</p>
 <?php include_once("piepagina.php"); ?>
