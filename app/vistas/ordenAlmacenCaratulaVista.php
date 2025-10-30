3d18f7259caa58d2b170d52e34c7386a42290133
BryanCachimuel <bryanloyo56@gmail.com>
2025-10-29 22:08:23 -0500

    Creando la vista principal de la Orden de Almacen en la cual se muestra el listado de las ordenes y sus respectivos botones para crear, mostrar , modificar y borrar

---
+++ app/vistas/ordenAlmacenCaratulaVista.php
@@ -0,0 +1,34 @@
+<?php include_once("encabezado.php"); ?>
+<div class="table-responsive">
+    <table class="table table-striped" width="100%">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>Orden Reparación</th>
+                <th>Costo</th>
+                <th>Fecha</th>
+                <th>Mostrar</th>
+                <th>Modificar</th>
+                <th>Borrar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            for ($i = 0; $i < count($datos['data']); $i++) {
+                print "<tr>";
+                print "<td class='text-start'>" . $datos["data"][$i]['id'] . "</td>";
+                print "<td class='text-start'>" . $datos["data"][$i]['idOrdenReparacion'] . "</td>";
+                print "<td class='text-start'>" . $datos["data"][$i]['costo'] . "</td>";
+                print "<td class='text-start'>" . $datos["data"][$i]['alta_dt'] . "</td>";
+                print "<td><a href='" . RUTA . "ordenAlmacen/mostrar/" . $datos["data"][$i]["id"] . "/" . $datos["pag"]["pagina"] . "' class='btn btn-warning'>Mostrar</a></td>";
+                print "<td><a href='" . RUTA . "ordenAlmacen/modificar/" . $datos["data"][$i]["id"] . "/" . $datos["pag"]["pagina"] . "' class='btn btn-info'>Modificar</a></td>";
+                print "<td><a href='" . RUTA . "ordenAlmacen/borrar/" . $datos["data"][$i]["id"] . "/" . $datos["pag"]["pagina"] . "' class='btn btn-danger'>Borrar</a></td>";
+                print "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+    <?php include_once("paginacion.php"); ?>
+    <a href="<?php print RUTA; ?>ordenAlmacen/alta" class="btn btn-success">
+        Dar de alta una Orden de Almacén</a>
+    <?php include_once("piepagina.php"); ?>
