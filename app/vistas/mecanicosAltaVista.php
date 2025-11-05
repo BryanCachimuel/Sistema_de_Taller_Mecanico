8cddcb048d03d1c0bce9d2b6d82b718b24485160
BryanCachimuel <bryanloyo56@gmail.com>
2025-11-04 21:42:20 -0500

    Mejorando las vistas de carátula y alta de mecánicos

--- app/vistas/mecanicosAltaVista.php
+++ app/vistas/mecanicosAltaVista.php
@@ -57,7 +57,7 @@
       </select>
     </div>
 
-    <div class="form-group text-start mt-3">
+    <div class="form-group text-start">
       <input type="hidden" name="id" id="id" value="<?php if (isset($datos['data']['id'])) { print $datos['data']['id']; } else { print ""; } ?>">
       <input type="hidden" name="pagina" id="pagina" value="<?php if (isset($datos['pagina'])) { print $datos['pagina']; } else { print "1"; } ?>">
       
