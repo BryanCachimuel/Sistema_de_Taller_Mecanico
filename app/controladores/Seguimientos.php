e50e0a0077a5be3b5787512ca585ed8e94a4d729
BryanCachimuel <bryanloyo56@gmail.com>
2025-11-07 22:37:47 -0500

    Modificando la función alta en donde se obtiene todos los datos ingresados por los inputs del formulario de alta de seguimientos

--- app/controladores/Seguimientos.php
+++ app/controladores/Seguimientos.php
@@ -26,98 +26,39 @@ class Seguimientos extends Controlador {
 	    $errores = array();
 	    if ($_SERVER['REQUEST_METHOD']=="POST") {
 	      //
-	      $id = $_POST['id'] ?? "";
-	      $idVehiculo = Helper::cadena($_POST['idVehiculo'] ?? "");
-	      $idMecanico = Helper::cadena($_POST['idMecanico'] ?? "");
-	      $fechaIngreso = Helper::cadena($_POST['fechaIngreso'] ?? "");
-	      $fechaSalida = Helper::cadena($_POST['fechaSalida'] ?? "");
-	      $kilometraje = Helper::numero($_POST['kilometraje'] ?? "");
-	      //
-	      $gato = empty($_POST['gato'])? "0": "1";
-	      $herramientas = empty($_POST['herramientas'])? "0": "1";
-	      $triangulos = empty($_POST['triangulos'])? "0": "1";
-	      $refaccion = empty($_POST['refaccion'])? "0": "1";
-	      $extintor = empty($_POST['extintor'])? "0": "1";
-	      $antena = empty($_POST['antena'])? "0": "1";
-	      $emblemas = empty($_POST['emblemas'])? "0": "1";
-	      $tapones = empty($_POST['tapones'])? "0": "1";
-	      $cables = empty($_POST['cables'])? "0": "1";
-	      $estereo = empty($_POST['estereo'])? "0": "1";
-	      $encendedor = empty($_POST['encendedor'])? "0": "1";
-	      $tapetes = empty($_POST['tapetes'])? "0": "1";
-	      //
+	      $idSeguimiento = $_POST['id'] ?? "";
+	      $idOrdenReparacion = Helper::cadena($_POST['idOrdenReparacion'] ?? "");
+	      $fecha = Helper::cadena($_POST['fecha'] ?? "");
+	      $observacion = Helper::cadena($_POST['observacion'] ?? "");
+
 	      $pagina = $_POST['pagina'] ?? "1";
 
-	      //
-	      // Validamos la información
-	      // 
-	      $hoy = date("Y-m-d");
-	      $hoy = new DateTime($hoy);
-	      if(empty($idVehiculo) || $idVehiculo=="void"){
-	        array_push($errores,"Debe de seleccionar un vehículo.");
-	      }
-	      if(empty($idMecanico) || $idMecanico=="void"){
-	        array_push($errores,"Debe de seleccioar un mecánico disponible.");
-	      }
-	      if(empty($fechaIngreso)){
-	        array_push($errores,"La fecha de ingreso es requerida.");
-	      } else if(Helper::fecha($fechaIngreso)==false){
-	      	array_push($errores,"El formato de la fecha de ingreso no es correcto.");
-	      } else {
-	      	//Verifica únicamente si es alta
-	      	$fechaIngreso_dt = new DateTime($fechaIngreso);
-	      	if($id==""){
-		      	$diff = $hoy->diff($fechaIngreso_dt);
-		      	if ($diff->invert) {
-		      		array_push($errores,"La fecha de ingreso no puede ser menor al día de hoy.");
-		      	}
-	      	}
-	      }
+		// validación de información
+	     if(empty($fecha)){
+	        array_push($errores,"La fecha del seguimiento es requerida.");
+	      } 
+	      if(Helper::fecha($fecha)==false){
+	      	array_push($errores,"El formato de la fecha no es correcto.");
+	      } 
 
-	      if(empty($fechaSalida)){
-	        array_push($errores,"El año del vehículo es requerido.");
-	      } else if(Helper::fecha($fechaSalida)==false){
-	      	array_push($errores,"El formato de la fecha de salida no es correcto.");
-	      } else {
-	      	$fechaSalida_dt = new DateTime($fechaSalida);
-	      	$diff = $fechaIngreso_dt->diff($fechaSalida_dt);
-	      	if($diff->invert){
-	      		array_push($errores,"La fecha de salida no puede ser inferior a la fecha de ingreso.");
-	      	}
-	      }
-	      if(empty($kilometraje)){
-	        array_push($errores,"El kilometraje es obligatorio.");
-	      } else if(intval($kilometraje)<0){
-	      	array_push($errores,"El kilometraje no puede ser negativo.");
-	      }
 	      //
 	      if (empty($errores)) { 
 			// Crear arreglo de datos
 			//
 			$data = [
-				"id" => $id,
-				"idVehiculo"=>$idVehiculo,
-				"idMecanico"=>$idMecanico,
-				"fechaIngreso"=>$fechaIngreso,
-				"fechaSalida"=>$fechaSalida,
-				"kilometraje"=>$kilometraje,
-				"gato"=>$gato,
-				"herramientas" => $herramientas,
-				"triangulos"=>$triangulos,
-				"refaccion"=>$refaccion,
-				"extintor"=>$extintor,
-				"antena"=>$antena,
-				"emblemas"=>$emblemas,
-				"tapones"=>$tapones,
-				"cables"=>$cables,
-				"estereo"=>$estereo,
-				"encendedor"=>$encendedor,
-				"tapetes"=>$tapetes
+				"id" => $idSeguimiento,
+				"id" => $idSeguimiento,
+				"idOrdenReparacion"=>$idOrdenReparacion,
+				"fecha"=>$fecha,
+				"observacion"=>$observacion
 			];    
-	        //Enviamos al modelo
-	        if(trim($id)===""){
+	         //Enviamos al modelo
+	        if(trim($idSeguimiento)===""){
 	          //Alta
-				if ($this->modelo->alta($data)) {
+	        	$id = $this->modelo->alta($data);
+				if ($id) {
+					// Imagenes
+					Helper::mostrar($id);
 					$this->mensaje(
 							"Alta de una orden de reparación", 
 							"Alta de una orden de reparación", 
