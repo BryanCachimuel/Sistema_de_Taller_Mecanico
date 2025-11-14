57c4c2ff570e005ffe0d12d810ca1c6aa4c286f0
BryanCachimuel <bryanloyo56@gmail.com>
2025-11-13 22:31:01 -0500

    Agregando el script para el encabezado de la factura

--- app/libs/Imprimir.php
+++ app/libs/Imprimir.php
@@ -18,7 +18,26 @@ class Imprimir extends fpdf {
     }
 
     // Cabecera de página
-    public function Header() {}
+    public function Header() {
+         // Logo
+        $this->Image('img/placeholder.jpg',150,8,50,40,"JPG");
+        // Arial bold 15
+        $this->SetFont('Arial','B',15);
+        $this->SetMargins(10,10,10);
+        // Título
+        $this->MultiCell(190,6,utf8_decode($this->razonSocial),0,'L');
+        // Salto de línea
+        $this->Line(10, 54, 200, 54);
+        $this->Ln(4);
+        // Título
+        $this->SetFont('Arial','',10);
+        $this->MultiCell(190,5,utf8_decode($this->cliente),0,'L');
+        $this->Line(10, 82, 200, 82);
+        $this->Ln(4);
+        $this->MultiCell(190,5,utf8_decode($this->vehiculo),0,'L');
+        $this->Line(10, 112, 200, 112);
+        $this->Ln(6);
+    }
 
     // Pie de página
     public function Footer() {}
