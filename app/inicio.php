3a276c87e52f884cc4c219128c998ad33ebd5207
BryanCachimuel <bryanloyo56@gmail.com>
2025-11-13 22:09:16 -0500

    Agregando los archivos necesarios para poder realizar la impresión de una factura mediante el uso de la librería fpdf

--- app/inicio.php
+++ app/inicio.php
@@ -43,6 +43,8 @@ define('ORDEN_FACTURADA',2);
 //
 date_default_timezone_set('America/Mexico_City');
 //
+require_once("libs/fpdf.php");
+require_once("libs/Imprimir.php");
 require_once("libs/Helper.php");
 require_once("libs/Sesion.php");
 require_once("libs/Controlador.php");
