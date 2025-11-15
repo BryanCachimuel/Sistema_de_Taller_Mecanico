413eb36f2f929b7c2f5ee2bdca019095b13895c3
BryanCachimuel <bryanloyo56@gmail.com>
2025-11-14 22:49:36 -0500

    Creando la función getIntegridadReferencial para conocer si existe una juntura entre dos entidades y si existe que se actualice los datos de la entidad respectiva

--- app/modelos/MecanicosModelo.php
+++ app/modelos/MecanicosModelo.php
@@ -30,13 +30,24 @@ class MecanicosModelo
 		return $this->db->queryNoSelect($sql);
 	}
 
-	public function bajaLogica(string $id):bool
-	{
+	public function bajaLogica(string $id):bool {
 		$salida = false;
 		$sql = "UPDATE mecanicos SET baja=1, baja_dt=(NOW()) WHERE id=".$id;
 		$salida = $this->db->queryNoSelect($sql);
 		return $salida;
 	}
+
+	public function getIntegridadReferencial($id) {
+		//
+		$ir_array = [0,0,0,0];
+		$sql = "SELECT COUNT(*) FROM ordenreparacion WHERE baja=0 AND idMecanico=".$id;
+		$salida = $this->db->query($sql);
+		$ir_array[1] = $salida["COUNT(*)"];
+		//
+		$ir_array[0] = $ir_array[1] + $ir_array[2] + $ir_array[3];
+		return $ir_array;
+	}
+
 
 	public function getId(string $id=''):array
 	{
