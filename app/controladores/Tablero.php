fe7353f16cb4fbd00d8adc247ea4c99e5845fc63
BryanCachimuel <bryanloyo56@gmail.com>
2025-11-16 22:00:46 -0500

    Creando la función respaldar para realizar un respaldo de la base de datos

--- app/controladores/Tablero.php
+++ app/controladores/Tablero.php
@@ -7,8 +7,7 @@ class Tablero extends Controlador
 	private $modelo = "";
 	private $sesion;
 	
-	function __construct()
-	{
+	function __construct() {
 		//Creamos sesion
 		$this->sesion = new Sesion();
 		if ($this->sesion->getLogin()) {
@@ -19,8 +18,7 @@ class Tablero extends Controlador
 		}
 	}
 
-	public function caratula()
-	{
+	public function caratula() {
 		$datos = [
 			"titulo" => "Sistema de taller mecánico",
 			"subtitulo" => $this->usuario["nombres"]." ".$this->usuario["apellidos"],
@@ -31,12 +29,26 @@ class Tablero extends Controlador
 		$this->vista("tableroCaratulaVista",$datos);
 	}
 
-	public function logout()
-	{
+	public function logout() {
 		if (isset($_SESSION['usuario'])) {
 			$this->sesion->finalizarLogin();
 		}
 		header("location:".RUTA);
 	}
+
+	public function respaldar() {
+		$m = "Cuidado: Este proceso realiza el respaldo de la base de datos. Puede tardar algunos minutos.";
+    	$this->mensaje(
+	  		"Respaldar la base de datos", 
+	  		"Respaldar la base de datos", 
+	  		$m, 
+	  		"tablero",
+	  		"danger",
+	  		"tablero/respaldarEjecutar/",
+	  		"success",
+	  		"Respaldar"
+	  	);
+	}
+
 }
 ?>
