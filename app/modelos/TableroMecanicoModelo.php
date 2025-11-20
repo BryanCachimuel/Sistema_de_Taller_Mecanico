e2c8396a94f07ec1c2833d99c4cc947627bec2f6
BryanCachimuel <bryanloyo56@gmail.com>
2025-11-19 22:04:21 -0500

    Creando la función getNumRegistros para obtener la cantidad de registros de una tabla en general de la base de datos

--- app/modelos/TableroMecanicoModelo.php
+++ app/modelos/TableroMecanicoModelo.php
@@ -8,6 +8,12 @@ class TableroMecanicoModelo {
 		$this->db = new MySQLdb();
 	}
 
+	public function getNumRegistros(string $tabla, string $id):int {
+		$sql = "SELECT COUNT(*) FROM ".$tabla." WHERE baja=0 AND idMecanico=".$id;
+		$salida = $this->db->query($sql);
+		return $salida["COUNT(*)"];
+	}
+
 	public function getTablaOrdenReparacion(int $inicio=1, int $tamano=0, string $id):array {
 		$sql = "SELECT o.id, o.idVehiculo, o.fechaIngreso, o.fechaSalida,  ";
 		$sql.= "CONCAT(v.marca,' ',v.modelo,' ',v.anio) as vehiculo, e.estado, o.estado as edo ";
