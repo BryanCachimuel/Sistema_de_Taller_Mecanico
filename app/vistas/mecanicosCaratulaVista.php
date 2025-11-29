124e80d08bb4641bcd1ae38fe63cffa338edb117
BryanCachimuel <bryanloyo56@gmail.com>
2025-11-28 23:16:03 -0500

    Actualizando los iconos en la tabla donde se listan los mecanicos

--- app/vistas/mecanicosCaratulaVista.php
+++ app/vistas/mecanicosCaratulaVista.php
@@ -19,14 +19,13 @@
       print "<td class='text-start'>".$datos["data"][$i]['nombre']."</td>";
       print "<td class='text-start'>".$datos["data"][$i]['tipo']."</td>";
       print "<td class='text-start'>".$datos["data"][$i]['estado']."</td>";
-      print "<td><a href='".RUTA."mecanicos/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'>Modificar</a></td>";
-      print "<td><a href='".RUTA."mecanicos/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'>Borrar</a></td>";
+      print "<td><a href='".RUTA."mecanicos/modificar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-info'><i class='fa-solid fa-marker'></i></a></td>";
+      print "<td><a href='".RUTA."mecanicos/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger'><i class='fa-solid fa-trash-can'></i></a></td>";
       print "</tr>";
     }
     ?>
   </tbody>
   </table>
   <?php include_once("paginacion.php"); ?> 
-<a href="<?php print RUTA; ?>mecanicos/alta" class="btn btn-success">
-  Dar de alta un mecánicos</a>
+<a href="<?php print RUTA; ?>mecanicos/alta" class="btn btn-success"><i class="fa-regular fa-square-plus"></i> Dar de alta un mecánicos</a>
 <?php include_once("piepagina.php"); ?>					
