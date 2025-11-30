13a43c7ad48785effcdcc4c40f1230c3297e3b85
BryanCachimuel <bryanloyo56@gmail.com>
2025-11-29 22:50:47 -0500

    Actualizando los iconos en la tabla donde se listan las ordenes de almacen

--- app/vistas/ordenAlmacenCaratulaVista.php
+++ app/vistas/ordenAlmacenCaratulaVista.php
@@ -21,18 +21,17 @@
       print "<td class='text-start'>$".number_format($datos["data"][$i]['costo'],2)."</td>";
       print "<td class='text-start'>".$datos["data"][$i]['alta_dt']."</td>";
       print "<td class='text-start'>".$datos["data"][$i]['estado']."</td>";
-      print "<td><a href='".RUTA."ordenAlmacen/desplegarOrdenAlmacen/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-warning'>Mostrar</a></td>";
+      print "<td><a href='".RUTA."ordenAlmacen/desplegarOrdenAlmacen/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-warning'><i class='fa-solid fa-eye'></i></a></td>";
       print "<td><a href='".RUTA."ordenAlmacen/borrar/".$datos["data"][$i]["id"]."/".$datos["pag"]["pagina"]."' class='btn btn-danger ";
       if ($datos["data"][$i]["idEstado"]==ORDEN_FACTURADA) {
         print " disabled ";
       }
-      print "'>Borrar</a></td>";
+      print "'><i class='fa-solid fa-trash-can'></i></a></td>";
       print "</tr>";
     }
     ?>
   </tbody>
   </table>
   <?php include_once("paginacion.php"); ?> 
-<a href="<?php print RUTA; ?>ordenAlmacen/alta" class="btn btn-success">
-  Dar de alta una orden de almacén</a>
+  <a href="<?php print RUTA; ?>ordenAlmacen/alta" class="btn btn-success"><i class="fa-regular fa-square-plus"></i> Dar de alta una orden de almacén</a>
 <?php include_once("piepagina.php"); ?>					
