0d5eb45fe92c2b076acd53da6e9342b94fb52ae3
BryanCachimuel <bryanloyo56@gmail.com>
2025-12-04 22:50:24 -0500

    Agregando iconos y mejorando la vista de olvido

--- app/vistas/loginOlvidoVista.php
+++ app/vistas/loginOlvidoVista.php
@@ -1,13 +1,13 @@
 <?php include_once("encabezado.php"); ?>
 <form action="<?php print RUTA; ?>login/olvido" method="POST">
 	<div class="form-group text-start">
-		<label for="correo">* Correo:</label>
+		<label for="correo">* <i class="fa-regular fa-envelope"></i> Correo:</label>
 		<input id="correo" name="correo" type="text" class="form-control" placeholder="Escribe tu correo electrónico">
 	</div>
 	<div class="form-group text-start my-2">
 		<input type="submit" value="Enviar" class="btn btn-success">
-		<a href="<?php print RUTA; ?>login" class="btn btn-success">Regresar</a>
+		<a href="<?php print RUTA; ?>login" class="btn btn-success"><i class="fa-solid fa-angles-left"></i> Regresar</a>
 	</div>
 </form>
-<p>Escribe tu correo registrado en el sistema</p>
+<p><strong><i class="fa-solid fa-tags"></i> Nota: Escribe tu correo registrado en el sistema</strong></p>
 <?php include_once("piepagina.php"); ?>					
